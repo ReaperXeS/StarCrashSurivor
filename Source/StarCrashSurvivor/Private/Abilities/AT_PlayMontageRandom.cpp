3cbf52c3fae8c66e13d819e8ab2e768e7a5fda77
Reaper XeS <reaperxes@gmail.com>
2023-02-11 16:31:15 -0500

    SCS-101 Upgrade to 5.1.1

--- Source/StarCrashSurvivor/Private/Abilities/AT_PlayMontageRandom.cpp
+++ Source/StarCrashSurvivor/Private/Abilities/AT_PlayMontageRandom.cpp
@@ -4,18 +4,18 @@
 #include "Abilities/AT_PlayMontageRandom.h"
 #include "AbilitySystemGlobals.h"
 
-UAT_PlayMontageRandom* UAT_PlayMontageRandom::CreatePlayMontageRandom(UGameplayAbility* OwningAbility, FName TaskInstanceName, UAnimMontage* MontageToPlay, float Rate, bool bStopWhenAbilityEnds, float AnimRootMotionTranslationScale, float StartTimeSeconds)
+UAT_PlayMontageRandom* UAT_PlayMontageRandom::CreatePlayMontageRandom(UGameplayAbility* OwningAbility, FName TaskInstanceName, UAnimMontage* MontageToPlayRandom, float Rate, bool bStopWhenAbilityEnds, float AnimRootMotionTranslationScale, float StartTimeSeconds)
 {
 	UAbilitySystemGlobals::NonShipping_ApplyGlobalAbilityScaler_Rate(Rate);
 
 	UAT_PlayMontageRandom* MyObj = NewAbilityTask<UAT_PlayMontageRandom>(OwningAbility, TaskInstanceName);
-	MyObj->MontageToPlay = MontageToPlay;
+	MyObj->MontageToPlay = MontageToPlayRandom;
 	MyObj->Rate = Rate;
-	if (MontageToPlay)
+	if (MontageToPlayRandom)
 	{
 		// Compute Random section
-		const uint8 SectionIndex = FMath::RandRange(0, MontageToPlay->CompositeSections.Num() - 1);
-		MyObj->StartSection = MontageToPlay->CompositeSections[SectionIndex].SectionName;
+		const uint8 SectionIndex = FMath::RandRange(0, MontageToPlayRandom->CompositeSections.Num() - 1);
+		MyObj->StartSection = MontageToPlayRandom->CompositeSections[SectionIndex].SectionName;
 	}
 	MyObj->AnimRootMotionTranslationScale = AnimRootMotionTranslationScale;
 	MyObj->bStopWhenAbilityEnds = bStopWhenAbilityEnds;
